43681c8f764c3c6b6c88b8002d4c227d6143fc4c
elif <elifdeniz@elif-MBP.home>
2023-12-05 13:37:23 +0300

    merged version2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import './assets/css/globals.css'
-import {RouterProvider} from  'react-router-dom'
-import routes from './routes/index'
+import ReactDOM from "react-dom/client";
+import "./assets/css/globals.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/index";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={routes}>
-        </RouterProvider>
-    )
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={routes}></RouterProvider>
+  </Provider>
+);
